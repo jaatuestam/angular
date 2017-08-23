fd121db0c36d33b14de16085fae41f943960fd7c
jorge.atuesta <jorge.atuesta@unionsoluciones.com.co>
2017-08-23 13:33:12 -0500

    COMMIT RUTAS CON PARAMETROS

--- SECCION 4/2. SPA/src/app/components/heroes/heroes.component.ts
+++ SECCION 4/2. SPA/src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HeroesService, Heroe} from '../../services/heroes.service';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
@@ -9,12 +9,18 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor( private _heroesService : HeroesService) {
+  constructor( private _heroesService : HeroesService,
+                private router:Router
+  ) {
 
   }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(index:number){
+    this.router.navigate(['/heroe',index]);
   }
 
 }
