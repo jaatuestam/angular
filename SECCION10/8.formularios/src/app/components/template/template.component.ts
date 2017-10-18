d18e79519c2bd95bd90ec67647da7d82616c39e9
jorge.atuesta <jorge.atuesta@unionsoluciones.com.co>
2017-10-18 17:17:32 -0500

    COMMIT DE ENVIO DE INFORMACION

--- SECCION10/8.formularios/src/app/components/template/template.component.ts
+++ SECCION10/8.formularios/src/app/components/template/template.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
@@ -7,11 +7,18 @@ import { Component } from '@angular/core';
 })
 export class TemplateComponent  {
 
+  usuario:Object = {
+    nombre: "Jorge",
+    apellido : "Atuesta",
+    correo: ""
+  }
 
   constructor() { }
 
-  guardar(){
-    console.log("Posteado")
+  guardar(forma:NgForm){
+    console.log("ngForm " , forma);
+    console.log("valor forma" , forma.value);
+    console.log("usuario " , this.usuario);
   }
 
 }
