4d9f3a6014354caca60847cb91c2a7e804b3ff56
jorge.atuesta <jorge.atuesta@unionsoluciones.com.co>
2017-10-26 13:30:24 -0500

    Commit validaciones personalizadas

--- SECCION10/8.formularios/src/app/components/data/data.component.ts
+++ SECCION10/8.formularios/src/app/components/data/data.component.ts
@@ -28,11 +28,13 @@ export class DataComponent {
       'correo' : new FormControl('',[Validators.required,
                                     Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
       'pasatiempos' : new FormArray([
-        new FormControl('Correr', Validators.required)
-      ])
+        new FormControl('Correr', Validators.required)]),
+      'password1' : new FormControl('',Validators.required),
+      'password2' : new FormControl()
     });
 
     // this.forma.setValue(this.usuario);
+    this.forma.controls['password2'].setValidators([Validators.required,this.noIgual.bind(this.forma)]);
   }
 
 
@@ -51,7 +53,17 @@ export class DataComponent {
   noHomero(control:FormControl):{[s:string]:boolean}{
     if(control.value === "homero"){
       return{
-        noHomero:false
+        nohomero:false
+      }
+    }
+    return null;
+  }
+
+  noIgual(control:FormControl):{[s:string]:boolean}{
+    let forma :any = this;
+    if(control.value !== forma.controls['password1'].value){
+      return{
+        noiguales:false
       }
     }
     return null;
