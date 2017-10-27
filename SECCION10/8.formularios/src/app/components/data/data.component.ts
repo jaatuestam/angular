00eb8b8654fd703fcb0695cf512b21d228bf4e72
jorge.atuesta <jorge.atuesta@unionsoluciones.com.co>
2017-10-27 13:13:14 -0500

    COMMIT FINAL DE FORMULARIOS

--- SECCION10/8.formularios/src/app/components/data/data.component.ts
+++ SECCION10/8.formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup ,FormControl, Validators,FormArray} from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-data',
@@ -29,12 +30,21 @@ export class DataComponent {
                                     Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
       'pasatiempos' : new FormArray([
         new FormControl('Correr', Validators.required)]),
+      'username' : new FormControl('',Validators.required,this.existeUsuario),
       'password1' : new FormControl('',Validators.required),
       'password2' : new FormControl()
     });
 
     // this.forma.setValue(this.usuario);
     this.forma.controls['password2'].setValidators([Validators.required,this.noIgual.bind(this.forma)]);
+
+    this.forma.controls['username'].valueChanges.subscribe((data)=>{
+      console.log(data);
+    });
+
+    this.forma.controls['username'].statusChanges.subscribe((data)=>{
+      console.log(data);
+    });
   }
 
 
@@ -73,6 +83,21 @@ export class DataComponent {
     (<FormArray>this.forma.controls['pasatiempos']).push(
       new FormControl('Dormir',Validators.required)
     );
+  }
+
+  existeUsuario(control:FormControl):Promise<any>|Observable<any>{
+    let promesa = new Promise(
+      (resolve,reject)=>{
+        setTimeout(()=>{
+          if(control.value === "jaatuestam"){
+            resolve({existe:true});
+          }else{
+            resolve(null);
+          }
+        },3000);
+      }
+    )
+    return promesa;
   }
 
 
