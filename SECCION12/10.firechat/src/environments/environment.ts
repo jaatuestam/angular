c1fb5c279d23547575fe85371b76c7f16b994bfe
jorge.atuesta <jorge.atuesta@unionsoluciones.com.co>
2017-11-07 17:53:13 -0500

    COMMIT INICIAL FIRECHAT

---
+++ SECCION12/10.firechat/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC9ZHe2FgF6xOLhaqz-2M5y9IRRxq8E4uU",
+    authDomain: "firechat-27355.firebaseapp.com",
+    databaseURL: "https://firechat-27355.firebaseio.com",
+    projectId: "firechat-27355",
+    storageBucket: "firechat-27355.appspot.com",
+    messagingSenderId: "217873218647"
+
+  }
+};
