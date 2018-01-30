5e1fad3b976336ef16c4f29fc80ec756b8cf10eb
jorge.atuesta <jorge.atuesta@unionsoluciones.com.co>
2018-01-30 17:56:40 -0500

    Commit rutas hijas

--- ANGULARADV/adminpro/src/app/pages/pages.module.ts
+++ ANGULARADV/adminpro/src/app/pages/pages.module.ts
@@ -5,6 +5,10 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 
+
+//Routes
+import  { PAGES_ROUTES } from './pages.routes';
+
 @NgModule({
   declarations: [
     PagesComponent,
@@ -13,7 +17,8 @@ import { SharedModule } from '../shared/shared.module';
     Graficas1Component
   ],
   imports: [
-    SharedModule
+    SharedModule,
+    PAGES_ROUTES
   ],
   exports: [
     DashboardComponent,
