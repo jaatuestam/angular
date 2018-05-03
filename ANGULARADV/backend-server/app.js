c9b59866020e722a0f56f2feb62ca698d248b463
jorge.atuesta <jaatuestam@gmail.com>
2018-05-03 17:12:49 -0500

    commit instalacion mongo

--- ANGULARADV/backend-server/app.js
+++ ANGULARADV/backend-server/app.js
@@ -1,8 +1,15 @@
 // Requires
 var express = require('express');
+var mongoose = require('mongoose');
 
 //inicializar variables
 var app = express();
+
+//conexion a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',(err,res) =>{
+  if(err) throw err;
+  console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online' );
+});
 
 //rutas
 app.get ('/',(req,res,next) => {
