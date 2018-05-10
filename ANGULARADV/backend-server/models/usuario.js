1f325fba43989aab64a2dfb17ab16e1d4a74dc8f
jorge.atuesta <jaatuestam@gmail.com>
2018-05-10 16:40:23 -0500

    CREACION DE USUARIO CON MEJORES VALIDACIONES

--- ANGULARADV/backend-server/models/usuario.js
+++ ANGULARADV/backend-server/models/usuario.js
@@ -1,4 +1,11 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+//roles validos
+var rolesValidos = {
+  values : ['ADMIN_ROLE', 'USER_ROLE'],
+  message : '{VALUE} no es un rol permitido'
+}
 
 var Schema = mongoose.Schema;
 
@@ -8,8 +15,9 @@ var usuarioSchema = new Schema({
     email: {type:String,unique:true,required : [true,'El correo es obligatorio']},
     password: {type:String,required : [true,'La contraseña es obligatorio']},
     img: {type:String,required : false},
-    role: {type:String,required : true, default:'USER_ROLE'}
+    role: {type:String,required : true, default:'USER_ROLE', enum:rolesValidos}
 });
 
+usuarioSchema.plugin(uniqueValidator,{message : '{PATH} debe ser único'})
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
