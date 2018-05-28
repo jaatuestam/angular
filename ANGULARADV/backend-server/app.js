2e2a8f04cbccaf1f3e24a94290f24f9add2a2816
jorge.atuesta <jaatuestam@gmail.com>
2018-05-28 13:54:34 -0500

    COMMIT DE SERVICIOS DE HOSPITALES

--- ANGULARADV/backend-server/app.js
+++ ANGULARADV/backend-server/app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json())
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
+var hospitalRoutes = require('./routes/hospital');
 
 
 //conexion a la base de datos
@@ -28,6 +29,7 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',(err,res) =>{
 //rutas
 app.use('/usuario',usuarioRoutes);
 app.use('/login',loginRoutes);
+app.use('/hospital',hospitalRoutes);
 app.use('/',appRoutes);
 
 //Escuchar peticiones
---
+++ ANGULARADV/backend-server/routes/hospital.js
@@ -0,0 +1,140 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+var middlewareAutenticacion = require('../middlewares/autenticacion');
+
+var app = express();
+
+var Hospital = require('../models/hospital');
+
+//========================================
+//obtener todos los hospitales
+//========================================
+app.get('/', (req,res) =>{
+  Hospital.find({}, 'nombre img').exec(
+    (err, hospitales)=> {
+      if(err){
+        return res.status(500).json({
+            ok : false,
+            mensaje: 'Error al consultar hospitales',
+            errores:err
+        });
+      }
+
+      res.status(200).json({
+        ok : true,
+        hospitales : hospitales
+      });
+
+    });
+});
+
+//========================================
+//actualizar un nuevo hospital
+//========================================
+app.put('/:id', middlewareAutenticacion.verificaToken, (req,res) =>{
+  var body = req.body;
+  var id = req.params.id;
+
+  Hospital.findById(id, (err, hospital) =>{
+    if(err){
+      return res.status(500).json({
+        ok:false,
+        mensaje: 'Error al buscar hospital',
+        errores: err
+      });
+    }
+
+    if(!hospital){
+      return res.status(400).json({
+        ok:false,
+        mensaje:`El hospital con el id ${id} no existe`,
+        errores: {message : 'No existe un hospital con ese ID'}
+      });
+    }
+
+    hospital.nombre = body.nombre;
+    hospital.usuario = req.usuario.id;
+
+    hospital.save ((err, hospitalGuardado) =>{
+      if(err){
+        return res.status(500).json({
+          ok:false,
+          mensaje:'error al actualizar hospital'
+        });
+      }
+
+      res.status(200).json({
+        ok:true,
+        hospital:hospitalGuardado
+      });
+
+    });
+
+  });
+});
+
+
+//========================================
+//guardar un nuevo hospital
+//========================================
+app.post('/', middlewareAutenticacion.verificaToken,(req,res) =>{
+  var body = req.body;
+
+  var hospital = new Hospital({
+    nombre : body.nombre,
+    img : body.img,
+    usuario : req.usuario.id
+  });
+
+  hospital.save((err,hospitalGuardado) =>{
+    if(err){
+      return res.status(400).json({
+          ok : false,
+          mensaje : 'error al crear hospital',
+          errores : err
+      });
+    }
+
+    res.status(201).json({
+      ok: true,
+      usuario: hospitalGuardado,
+      usuariotoken : req.usuario
+    });
+
+  });
+});
+
+
+//========================================
+//borrar un hospital
+//========================================
+app.delete('/:id', middlewareAutenticacion.verificaToken,(req,res) =>{
+  var id = req.params.id;
+
+  Hospital.findByIdAndRemove(id,(err, hospitalBorrado) =>{
+      if(err){
+        return res.status(500).json({
+          ok:false,
+          mensaje:'error al borrar usuario',
+          errores : err
+        });
+      }
+
+      if(!hospitalBorrado){
+        return res.status(400).json({
+          ok:false,
+          mensaje: `No existe el hospital con el id ${id}`,
+          errores : {mensaje: `No existe el hospital con el id`}
+        });
+      }
+
+      res.status(200).json({
+        ok:true,
+        hospital : hospitalBorrado
+      })
+  });
+});
+
+module.exports  = app;
