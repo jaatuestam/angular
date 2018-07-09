60e56cc5756588ec632fe24c697897ab6f4eb2b3
jorge.atuesta <jaatuestam@gmail.com>
2018-07-09 17:06:45 -0500

    ajuste

--- ANGULARADV/backend-server/app.js
+++ ANGULARADV/backend-server/app.js
@@ -19,6 +19,7 @@ var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
 var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
 
 
 //conexion a la base de datos
@@ -32,6 +33,7 @@ app.use('/usuario',usuarioRoutes);
 app.use('/login',loginRoutes);
 app.use('/hospital',hospitalRoutes);
 app.use('/medico',medicoRoutes);
+app.use('/busqueda',busquedaRoutes);
 app.use('/',appRoutes);
 
 //Escuchar peticiones
