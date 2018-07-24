47053683cc72cfc7ed192e682f0d76ca6b7cce7f
jorge.atuesta <jaatuestam@gmail.com>
2018-07-24 09:20:05 -0500

    commit busqueda todo

--- ANGULARADV/backend-server/routes/busqueda.js
+++ ANGULARADV/backend-server/routes/busqueda.js
@@ -4,6 +4,7 @@ var app = express();
 
 var Hospital = require('../models/hospital');
 var Medico = require('../models/medico');
+var Usuario = require('../models/usuario');
 
 app.get ('/todo/:busqueda',(req,res,next) => {
 
@@ -11,14 +12,17 @@ app.get ('/todo/:busqueda',(req,res,next) => {
 
   var regex = new RegExp(busqueda, 'i');
 
-  buscarHospitales(busqueda,regex).then(hospitales =>{
-    res.status(200).json({
-      ok:true,
-      hospitales:hospitales
-    });
-  })
-
-
+  Promise.all([buscarHospitales(busqueda,regex),
+              buscarMedicos(busqueda, regex),
+              buscarUsuarios(busqueda, regex)
+            ]).then(respuesta =>{
+              res.status(200).json({
+                ok:true,
+                hospitales:respuesta[0],
+                medicos:respuesta[1],
+                usuarios:respuesta[2]
+              });
+            });
 });
 
 
@@ -26,15 +30,48 @@ function buscarHospitales(busqueda, regex){
 
     return new Promise((resolve, reject) =>{
 
-      Hospital.find({nombre : regex}, (err,hospitales) =>{
+      Hospital.find({nombre : regex})
+                .populate('usuario','nombre email')
+                .exec((err,hospitales) =>{
+                  if(err){
+                    reject('error al buscar hospitales' , err);
+                  }else{
+                    resolve(hospitales);
+                  }
+                });
+    });
+}
+
+function buscarMedicos(busqueda, regex){
+
+    return new Promise((resolve, reject) =>{
+
+      Medico.find({nombre : regex})
+              .populate('usuario','nombre email')
+              .exec((err,medicos) =>{
         if(err){
-          reject('error al buscar hospitales' , err);
+          reject('error al buscar medico' , err);
         }else{
-          resolve(hospitales);
+          resolve(medicos);
         }
       });
     });
+}
 
+function buscarUsuarios(busqueda, regex){
+
+    return new Promise((resolve, reject) =>{
+
+      Usuario.find({}, 'nombre email role')
+              .or([{'nombre':regex}, {'email':regex}])
+              .exec((err,usuarios) =>{
+                      if(err){
+                        reject('error al buscar usuarios' , err);
+                      }else{
+                        resolve(usuarios);
+                      }
+                    });
+    });
 }
 
 module.exports = app;
