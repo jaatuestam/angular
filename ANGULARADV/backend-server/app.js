d2e24c10865068acadcf8512b62908498a094bff
jorge.atuesta <jaatuestam@gmail.com>
2018-07-25 09:17:34 -0500

    servicio de consulta de imagenes

--- ANGULARADV/backend-server/app.js
+++ ANGULARADV/backend-server/app.js
@@ -21,6 +21,7 @@ var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
 var busquedaRoutes = require('./routes/busqueda');
 var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 
 //conexion a la base de datos
@@ -36,6 +37,7 @@ app.use('/hospital',hospitalRoutes);
 app.use('/medico',medicoRoutes);
 app.use('/busqueda',busquedaRoutes);
 app.use('/upload',uploadRoutes);
+app.use('/img',imagenesRoutes);
 app.use('/',appRoutes);
 
 //Escuchar peticiones
