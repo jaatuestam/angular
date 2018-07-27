bc172d538774acf2949b392ae040fa0a6ccea557
jorge.atuesta <jaatuestam@gmail.com>
2018-07-27 17:58:34 -0500

    COMMIT LOGIN CON GOOGLE

--- ANGULARADV/backend-server/config/config.js
+++ ANGULARADV/backend-server/config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-el-seed';
+
+//google
+module.exports.CLIENT_ID = '82127856595-mq08uvhpkmnjumo52f9uc1snt6vpjoio.apps.googleusercontent.com';
--- ANGULARADV/backend-server/routes/login.js
+++ ANGULARADV/backend-server/routes/login.js
@@ -7,6 +7,55 @@ var SEED = require('../config/config').SEED;
 var app = express();
 var Usuario = require('../models/usuario');
 
+//google
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+
+//========================================
+//login de google
+//========================================
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+
+  return {
+    nombre:payload.name,
+    email: payload.email,
+    img: payload.picture,
+    google:true
+  }
+  // const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+}
+
+
+app.post('/google',async (req,res) =>{
+
+  var token = req.body.token;
+
+  var googleUser = await verify(token).catch(err =>{
+    return res.status(403).json({
+      ok: false,
+      mensaje: 'token no valido'
+    });
+  });
+
+  return res.status(200).json({
+    ok: true,
+    mensaje: 'ok'
+    googleUser : googleUser
+  });
+});
+
+
 
 //========================================
 //login de usuario
