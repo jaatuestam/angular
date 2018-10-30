ce7360e8af897388be02950d4925b033dcd28ccb
jorge.atuesta <jaatuestam@gmail.com>
2018-10-30 10:15:44 -0500

    commit de directiva completa

--- SECCION18/fotosfirebase/src/app/directives/ng-dro-file.directive.ts
+++ SECCION18/fotosfirebase/src/app/directives/ng-dro-file.directive.ts
@@ -14,11 +14,50 @@ export class NgDroFileDirective {
   @HostListener('dragover', ['$event'])
   public onDragEnter(event:any){
     this.mouseSobre.emit(true);
+    this._prevenirDetener(event);
   }
   
   @HostListener('dragleave', ['$event'])
   public onDragLeave(event:any){
     this.mouseSobre.emit(false);
+  }
+
+  @HostListener('drop', ['$event'])
+  public onDrop(event:any){
+
+    const transferencia = this._getTransferencia(event);
+
+    if(!transferencia){
+      return;
+    }
+
+    this._extraerArchivos(transferencia.files);
+    this._prevenirDetener(event);
+
+    this.mouseSobre.emit(false);
+  }
+
+  private _extraerArchivos(archivosLista : FileList){
+    // console.log(archivosLista);
+
+    //Pasar un objeto a un arreglo
+    for(const propiedad in Object.getOwnPropertyNames(archivosLista)){
+      const archivoTemporal = archivosLista[propiedad];
+      
+      if(this._puedeCargarse(archivoTemporal)){
+        const nuevoArchivo = new FileItem(archivoTemporal);
+        this.archivos.push(nuevoArchivo);
+
+      }
+    }
+    console.log("arreglo", this.archivos);
+
+    
+    
+  }
+
+  private _getTransferencia(event:any){
+    return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer;
   }
 
   //Validaciones
@@ -30,18 +69,17 @@ export class NgDroFileDirective {
   }
 
 
-  private _prevenirApertura(event){
+  private _prevenirDetener(event){
     event.preventDefault();
     event.stopPropagation();
   }
 
-  private _archivoYaCargado(nombreArchivo:String){
+  private _archivoYaCargado(nombreArchivo:string){
     for (const archivo of this.archivos) {
       if(archivo.nombreArchivo === nombreArchivo){
         console.log("El archivo " + nombreArchivo + " ya fue agregado");
         return true;
       }
-      return false;
     }
   }
 
