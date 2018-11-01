39849e6942669a7459ae055b1c48196736372367
jorge.atuesta <jaatuestam@gmail.com>
2018-11-01 17:33:34 -0500

    commit final drag and drop

--- SECCION21/virtualscroll/src/app/app.module.ts
+++ SECCION21/virtualscroll/src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './virtual/virtual.component';
+import { DragComponent } from './drag/drag.component';
+import { PaisesComponent } from './paises/paises.component';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VirtualComponent
+    VirtualComponent,
+    DragComponent,
+    PaisesComponent
   ],
   imports: [
     BrowserModule,
-    ScrollingModule
+    ScrollingModule,
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
